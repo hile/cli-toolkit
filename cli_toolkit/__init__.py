29bdafbd4e7ea34397ecc53960b12ec725aec67d
Ilkka Tuohela <hile@iki.fi>
2022-03-13 08:05:14 +0200

    Remove __version__ from module __init__, version info now in pyproject.toml only

--- cli_toolkit/__init__.py
+++ cli_toolkit/__init__.py
@@ -5,4 +5,3 @@ This module implements python modules to implement command line tools,
 extending python argparse.ArgumentParser to make writing nested CLI
 commands easier.
 """
-__version__ = '2.1.0'
